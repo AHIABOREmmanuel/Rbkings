ade635d70fc125b6171debb3ba1bd5a99557f8a2
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-06-02 12:28:13 +0000

    Correction des conflits de dépendances

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { GoogleMapsModule } from '@angular/google-maps';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,8 +28,7 @@ import { ContactComponent } from './components/contact/contact.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    GoogleMapsModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
